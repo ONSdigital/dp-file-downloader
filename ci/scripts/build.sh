975eeb7d77e52c904a831a61aa9f2bca269c9928
Jon Dewi Jones <jondewijones@gmail.com>
2020-10-26 12:50:44 +0000

    Remove gopath

--- ci/scripts/build.sh
+++ ci/scripts/build.sh
@@ -4,7 +4,7 @@ cwd=$(pwd)
 
 export GOPATH=$cwd/go
 
-pushd $GOPATH/src/github.com/ONSdigital/dp-file-downloader
+pushd $GOPATH/dp-file-downloader
   make build && mv build/$(go env GOOS)-$(go env GOARCH)/* $cwd/build
   cp Dockerfile.concourse $cwd/build
 popd
--- ci/scripts/unit.sh
+++ ci/scripts/unit.sh
@@ -2,6 +2,6 @@
 
 export GOPATH=$(pwd)/go
 
-pushd $GOPATH/src/github.com/ONSdigital/dp-file-downloader
+pushd $GOPATH/dp-file-downloader
   make test
 popd
